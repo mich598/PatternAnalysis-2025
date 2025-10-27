c7396d02996b0827e0e5780f3428e56c32bb6477
Michael <michaelchen598@gmail.com>
2025-10-27 13:58:41 +1000

    Setup initial file structure with dataset.py, modules.py, predict.py, train.py, utils.py and README.md

---
+++ recognition/TimeGAN_LOB_AMZN_10_47480063/README.md
@@ -0,0 +1,2 @@
+Generative Time Series Model using TimeGAN that generates synthetic sequences of limit order books
+for amazon level 10 data
---
+++ recognition/TimeGAN_LOB_AMZN_10_47480063/dataset.py
@@ -0,0 +1,71 @@
+"""
+containing the data loader for loading and preprocessing your data
+
+Reference: Jinsung Yoon, Daniel Jarrett, Mihaela van der Schaar, 
+"Time-series Generative Adversarial Networks," 
+Neural Information Processing Systems (NeurIPS), 2019.
+
+Github Link: https://github.com/jsyoon0823/TimeGAN/blob/master/data_loading.py
+
+data_loading.py
+
+(0) MinMaxScaler: Min Max normalizer
+(1) real_data_loading: Load and preprocess real data
+  - stock_data: https://finance.yahoo.com/quote/GOOG/history?p=GOOG
+"""
+
+## Necessary Packages
+import numpy as np
+
+
+def MinMaxScaler(data):
+  """Min Max normalizer.
+  
+  Args:
+    - data: original data
+  
+  Returns:
+    - norm_data: normalized data
+  """
+  numerator = data - np.min(data, 0)
+  denominator = np.max(data, 0) - np.min(data, 0)
+  norm_data = numerator / (denominator + 1e-7)
+  return norm_data
+    
+
+def real_data_loading (data_name, seq_len):
+  """Load and preprocess real-world datasets.
+  
+  Args:
+    - data_name: stock or energy
+    - seq_len: sequence length
+    
+  Returns:
+    - data: preprocessed data.
+  """  
+  assert data_name in ['stock','energy']
+  
+  if data_name == 'stock':
+    ori_data = np.loadtxt('data/stock_data.csv', delimiter = ",",skiprows = 1)
+  elif data_name == 'energy':
+    ori_data = np.loadtxt('data/energy_data.csv', delimiter = ",",skiprows = 1)
+        
+  # Flip the data to make chronological data
+  ori_data = ori_data[::-1]
+  # Normalize the data
+  ori_data = MinMaxScaler(ori_data)
+    
+  # Preprocess the dataset
+  temp_data = []    
+  # Cut data by sequence length
+  for i in range(0, len(ori_data) - seq_len):
+    _x = ori_data[i:i + seq_len]
+    temp_data.append(_x)
+        
+  # Mix the datasets (to make it similar to i.i.d)
+  idx = np.random.permutation(len(temp_data))    
+  data = []
+  for i in range(len(temp_data)):
+    data.append(temp_data[idx[i]])
+    
+  return data
